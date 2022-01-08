4c34efd636218c8bf0418c6683f3d0357b7ae088
916BGAI <33173240+916BGAI@users.noreply.github.com>
2022-01-08 15:09:37 +0800

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "125",
+    "top": "20",
+    "key": "543c7fe257bd43af8d9b9bfb39d2abf5"
+  }
+}
